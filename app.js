847df5c95b12d0bec4bca899705673fd466fd178
Maike Castilho <maikecastilho18@gmail.com>
2023-02-17 16:51:30 -0300

    responsivo

--- app.js
+++ app.js
@@ -2,4 +2,12 @@ function converter(num) {
     var dec = parseInt(num)
     var bin = dec.toString(2)
     document.getElementById("res").innerText=bin
+
+ 
 }
+
+// function resetar(){
+//     document.getElementById("frm").reset();
+//     $("#result").reset();
+ 
+// }
